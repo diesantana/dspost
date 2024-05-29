11409af607c6403063c1b75fb6be1bf75c1bb4bf
Diego Santana <3c.santana@gmail.com>
2024-05-29 09:55:12 -0300

    feat: Cria o atributos posts

--- src/main/java/com/devsuperior/dspost/models/entities/User.java
+++ src/main/java/com/devsuperior/dspost/models/entities/User.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dspost.models.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
@@ -11,6 +14,9 @@ public class User {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -43,6 +49,10 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
 	}
 
 	@Override
