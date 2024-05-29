34eec1b558ca8aadad4ef6fcbea59805cc981a2c
Diego Santana <3c.santana@gmail.com>
2024-05-29 13:47:11 -0300

    feat(UserController): implementa o método insert

--- src/main/java/com/devsuperior/dspost/controllers/UserController.java
+++ src/main/java/com/devsuperior/dspost/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dspost.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dspost.models.dto.UserDTO;
 import com.devsuperior.dspost.services.UserService;
@@ -29,6 +33,14 @@ public class UserController {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		UserDTO result = userService.findById(id);
 		return ResponseEntity.ok(result);
+	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO dto) {
+		UserDTO result = userService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(result.getId()).toUri();
+		return ResponseEntity.created(uri).body(result);
 	}
 	
 	
