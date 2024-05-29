af19d4d14a78f11b6704dccaf4887af8522a398f
Diego Santana <3c.santana@gmail.com>
2024-05-29 11:56:15 -0300

    feat(UserController): implementa o FindById

--- src/main/java/com/devsuperior/dspost/controllers/UserController.java
+++ src/main/java/com/devsuperior/dspost/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,13 @@ public class UserController {
 		List<UserDTO> result = userService.findAll();
 		return ResponseEntity.ok(result);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		UserDTO result = userService.findById(id);
+		return ResponseEntity.ok(result);
+	}
+	
+	
 
 }
