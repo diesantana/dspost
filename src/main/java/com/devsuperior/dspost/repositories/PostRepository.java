7037e627d58a5245a4c50e3a39d1d7f29437f927
Diego Santana <3c.santana@gmail.com>
2024-05-30 11:34:21 -0300

    feat(PostRepository): Implementa a busca por title com Query Operators

--- src/main/java/com/devsuperior/dspost/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/dspost/repositories/PostRepository.java
@@ -3,9 +3,14 @@ package com.devsuperior.dspost.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.devsuperior.dspost.models.entities.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{'title': { $regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
