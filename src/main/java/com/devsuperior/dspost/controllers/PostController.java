4efc556fe7e88f28e61bb7bb2dabb58613e939fa
Diego Santana <3c.santana@gmail.com>
2024-05-30 08:37:59 -0300

    feat(PostController): Implementa a busca por title

--- src/main/java/com/devsuperior/dspost/controllers/PostController.java
+++ src/main/java/com/devsuperior/dspost/controllers/PostController.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dspost.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dspost.models.dto.PostDTO;
@@ -20,6 +23,12 @@ public class PostController {
 	@GetMapping("/{id}")
 	public ResponseEntity<PostDTO> findById(@PathVariable String id) {
 		PostDTO result = postService.findById(id);
+		return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<PostDTO>> findbyTitle(@RequestParam(name = "text", defaultValue = "") String text) {
+		List<PostDTO> result = postService.findByTitle(text);
 		return ResponseEntity.ok(result);
 	}
 }
