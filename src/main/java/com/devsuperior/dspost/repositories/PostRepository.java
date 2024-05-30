bb85dd02bfb612d80446ae4ffe8367cd26db5c67
Diego Santana <3c.santana@gmail.com>
2024-05-30 08:36:39 -0300

    feat(PostRepository): Implementa a busca por title

--- src/main/java/com/devsuperior/dspost/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/dspost/repositories/PostRepository.java
@@ -1,9 +1,11 @@
 package com.devsuperior.dspost.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.devsuperior.dspost.models.entities.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
